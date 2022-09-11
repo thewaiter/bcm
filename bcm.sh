e5bed5e441128b889cab94cd4885a22ac33539b0
thewaiter <s.uram@centrum.sk>
2022-09-11 13:44:50 +0200

    Y/N question added

--- bcm.sh
+++ bcm.sh
@@ -10,11 +10,29 @@ bcmwl () {
    sudo apt purge firmware-b43-installer
    sudo apt install bcmwl-kernel-source
 }
- 
+
+installer () {
+   case $1 in
+      b43)   b43;;
+      bcmwl) bcmwl;;
+   esac
+}
+
+question () {
+ while true; do
+    read -p "Do you wish to install suitable bcm driver [y/n]? " yn
+    case $yn in
+        [Yy]* ) installer $1; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done 
+}
+
 unknown () {
    echo 'Unidentified Wifi card. Please check Bodhi wiki!'
 }
- 
+
 input=$(lspci -nn -d 14e4:) # exceutes command and incializes variable
 token=$(echo "$input" | awk -F"14e4:" '{print $2}') # find token
 
@@ -25,49 +43,49 @@ var=$(echo "${var//)/}")                            # replace )
 
 echo "Your Wifi device: bcm [14e14:$token"
 case $var in
-    1713)           b43;;
-    4301)           b43;;
-    4306rev02)      b43;;
-    4306rev03)      b43;;
-    4307)           b43;;
-    4311)           b43;;
-    4311rev01)      b43;;
-    4312)           b43;;
-    4313)           b43;;
-    4315)           b43;;
-    4315rev01)      b43;;
-    4318)           b43;;
-    4318rev02)      b43;;
-    4319)           b43;;
-    4320rev02)      b43;;
-    4320rev03)      b43;;
-    4321)           b43;;
-    4324)           b43;;
-    4325)           b43;;
-    4328)           b43;;
-    4328rev03)      bcmwl;;
-    4329)           bcmwl;;
-    432a)           bcmwl;;
-    432b)           bcmwl;;
-    432c)           bcmwl;;
-    432d)           bcmwl;;
-    4331)           b43;;
-    4335)           b43;;
-    4350)           b43;;
-    4353rev01)      b43;;
-    4358)           bcmwl;;
-    4359)           b43;;
-    4360)           b43;;
-    4365)           b43;;
-    4365rev01)      bcmwl;;
-    43a0)           bcmwl;;
-    43a3)           b43;;
-    43barev01)      b43;;
-    43b1)           bcmwl;;
-    43b1rev03)      bcmwl;;
-    43c3rev04)      b43;;
-    4727)           bcmwl;;
-    a962)           b43;;
+    1713)           question "b43";;
+    4301)           question "b43";;
+    4306rev02)      question "b43";;
+    4306rev03)      question "b43";;
+    4307)           question "b43";;
+    4311)           question "b43";;
+    4311rev01)      question "b43";;
+    4312)           question "b43";;
+    4313)           question "b43";;
+    4315)           question "b43";;
+    4315rev01)      question "b43";;
+    4318)           question "b43";;
+    4318rev02)      question "b43";;
+    4319)           question "b43";;
+    4320rev02)      question "b43";;
+    4320rev03)      question "b43";;
+    4321)           question "b43";;
+    4324)           question "b43";;
+    4325)           question "b43";;
+    4328)           question "b43";;
+    4328rev03)      question "bcmwl";;
+    4329)           question "bcmwl";;
+    432a)           question "bcmwl";;
+    432b)           question "bcmwl";;
+    432c)           question "bcmwl";;
+    432d)           question "bcmwl";;
+    4331)           question "b43";;
+    4335)           question "b43";;
+    4350)           question "b43";;
+    4353rev01)      question "b43";;
+    4358)           question "bcmwl";;
+    4359)           question "b43";;
+    4360)           question "b43";;
+    4365)           question "b43";;
+    4365rev01)      question "bcmwl";;
+    43a0)           question "bcmwl";;
+    43a3)           question "b43";;
+    43barev01)      question "b43";;
+    43b1)           question "bcmwl";;
+    43b1rev03)      question "bcmwl";;
+    43c3rev04)      question "b43";;
+    4727)           question "bcmwl";;
+    a962)           question "b43";;
     *)              unknown;;
 esac
  
