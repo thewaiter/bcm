20167e67c8f0ea35edef4b2741123240386ed891
thewaiter <s.uram@centrum.sk>
2024-03-09 13:48:30 +0100

    Some Copyright lines no need

--- bcm.sh
+++ bcm.sh
@@ -13,45 +13,23 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 # General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with "set-background-bodhi"; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-
-
 
 b43 () {
    sudo apt update
-   if [ "$platform" == "Ubuntu" ]; then
-        sudo apt purge bcmwl-kernel-source
-   fi 
-   if [ "$platform" == "Debian" ]; then
-       sudo apt purge broadcom-sta-dkms
-   fi
-   sudo apt install firmware-b43-installer
+   sudo apt purge bcmwl-kernel-source
+   sudo apt install firmware-b43-installer && sudo apt install linux-firmware
 }
 
 b43legacy () {
    sudo apt update
-   if [ "$platform" == "Ubuntu" ]; then
-        sudo apt purge bcmwl-kernel-source
-   fi 
-   if [ "$platform" == "Debian" ]; then
-       sudo apt purge broadcom-sta-dkms
-   fi
-   sudo apt install firmware-b43legacy-installer
+   sudo apt purge bcmwl-kernel-source
+   sudo apt install firmware-b43legacy-installer && sudo apt install linux-firmware
 }
  
 bcmwl () {
    sudo apt update
    sudo apt purge firmware-b43-installer
-   sudo apt purge firmware-b43legacy-installer
-   if [ "$platform" == "Ubuntu" ]; then
-        sudo apt install bcmwl-kernel-source
-   fi 
-   if [ "$platform" == "Debian" ]; then
-       sudo apt install firmware-iwlwifi
-       sudo apt install broadcom-sta-dkms
-   fi
+   sudo apt install bcmwl-kernel-source
 }
 
 installer () {
@@ -77,11 +55,9 @@ unknown () {
    echo 'Unidentified Wifi card. Please check Bodhi wiki!'
 }
 
-platform=$(lsb_release -si | awk -F"\t" '{print $1}')
-echo $'Your platform: \e[1;31m'$platform$'\e[0m'
-
 input=$(lspci -nn -d 14e4:) # executes command and incializes variable
 token=$(echo "$input" | awk -F"14e4:" '{print $2}') # find token
+
 var=$(echo "$token" | sed 's/[][()]//g' | sed 's/ //g')
 
 if [ -z "$token" ]
